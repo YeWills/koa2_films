02d44d31e6c8582f45be1d0fdf11bd733cda9a47
HZ-1 <601661706@qq.com>
2019-02-28 00:45:46 +0800

    10-1和10-2,可正常运行，npm start，启动后台服务，编译前台代码，然后在浏览器上访问整个页面，实现了整个前后台链路的调通，详见readme..

--- server/index.js
+++ server/index.js
@@ -4,11 +4,9 @@ const views = require('koa-views')
 const { resolve } = require('path')
 const { connect, initSchemas, initAdmin } = require('./database/init')
 const R = require('ramda')
-const MIDDLEWARES = ['router']
+const MIDDLEWARES = ['router', 'parcel']
 
-// import {abc} from './abc.js'
-// console.log('225566')
-// console.log(abc)
+
 const useMiddlewares = (app) => {
   R.map(
     R.compose(
@@ -22,22 +20,15 @@ const useMiddlewares = (app) => {
 }
 
 ;(async () => {
-  // 连接数据库
   await connect()
 
-  // 初始化scama
   initSchemas()
 
-  // 查数据？
   await initAdmin()
 
-  // 爬取电影数据
   // require('./tasks/movie')
-  // // 对爬取的数据组装？
   // require('./tasks/api')
-  // // 对爬取的数据组装？
   // require('./tasks/trailer')
-  // // 对爬取的数据上传到七牛
   // require('./tasks/qiniu')
 
   const app = new Koa()
